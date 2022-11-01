08d6de2f8c37b822577f6dbcf4c8bec1a98beaaa
Shrinitha <shrinitha410@gmail.com>
2022-11-02 03:45:18 +0530

    version 1.4

--- camera_flask_app.py
+++ camera_flask_app.py
@@ -70,8 +70,11 @@ def gen_frames():  # generate frame by frame from camera
 @app.route('/')
 def index():
     return render_template('index1.html')
-    
-    
+
+@app.route('/demo')
+def demo():
+    return render_template('demo.html')
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
